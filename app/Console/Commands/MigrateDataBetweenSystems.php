0be25b7132274048d06ccfdef54cd1d4e9685080
Nigel Lima <nigelnaiguel.comp@gmail.com>
2018-08-01 20:54:20 +0900

    Updates 4

--- app/Console/Commands/MigrateDataBetweenSystems.php
+++ app/Console/Commands/MigrateDataBetweenSystems.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-use App\Http\Controllers\MigrationMController;
+use App\Http\Controllers\MigrationController;
 
 class MigrateDataBetweenSystems extends Command
 {
